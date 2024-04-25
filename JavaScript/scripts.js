4a7f70f40171dc1a91fdb182b4a4dee71487d290
Guto <gutopaganella@gmail.com>
2024-04-24 21:57:27 -0300

    Reconhecimento do segundo seletc, vendo qua a currency selecionada e convertendo para a moeda em questão. no proximo ira trocar tambám as bandeiras e o nome de cada um.

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -1,30 +1,68 @@
 const convertButton = document.querySelector(".convert-button")
-
-    function convertValues(){
-        const inputCurrencyValue = document.querySelector(".input-currency").value // input de valor digitado.        
-        const currencyValueToConvert = document.querySelector(".currency-value-to-converted") // Valor a ser convertido. 
-        const currencyValueConverted = document.querySelector(".currency-value") // valor Convertido.
-        
+const currencySelectToConvert = document.getElementById("select-to-convert")
+const currencySelect = document.getElementById("select-converted")
 
 
-        const dolarToday = 5.2
-
-        const convertedValue = inputCurrencyValue / dolarToday
-
-        
-        currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", 
-        {   style: "currency", 
-            currency: "BRL"
-        }).format(inputCurrencyValue)
-        
-        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", { 
-            style: "currency", 
-            currency: "USD"
-        }).format(convertedValue)
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value // input de valor digitado.        
+    const currencyValueToConvert = document.querySelector(".currency-value-to-converted") // Valor a ser convertido. 
+    const currencyValueConverted = document.querySelector(".currency-value") // valor Convertido.
 
 
-        console.log(convertedValue)
+    console.log(currencySelectToConvert.value)
+    console.log(currencySelect.value)
+
+    /*****************************************************************
+    *********************  Tabela de troca    ************************
+    *****************************************************************/
+    const dolarTodayEuaReal = 5.13  //.................. EUA para Real
+    const euroTodayReal = 5.49      //................. Euro para Real 
+    const dolarTodayAusReal = 3.33  //.................. AUS para Real
+
+    /*
+    const euroEUA = 1.07            //................. Euro para EUA
+    const dolarTodayAUSEUA = 0.65   //.................. EUA para AUS
+    const euroTodayAUS = 1.65       //................. Euro para AUS
+    */
+
+    if (currencySelectToConvert.value == "real") {
+        {
+            currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL"
+            }).format(inputCurrencyValue)
+        }
+
+        if (currencySelect.value == "real") {
+            currencyValueConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL"
+            }).format(inputCurrencyValue)
+        }
+
+        if (currencySelect.value == "dolarUS$") {
+            currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD"
+            }).format(inputCurrencyValue / dolarTodayEuaReal)
+        }
+
+        if (currencySelect.value == "dolarAU$") {
+            currencyValueConverted.innerHTML = new Intl.NumberFormat("en-AU", {
+                style: "currency",
+                currency: "AUS"
+            }).format(inputCurrencyValue / dolarTodayAusReal)
+        }
+
+        if (currencySelect.value == "euro") {
+            currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+                style: "currency",
+                currency: "EUR"
+            }).format(inputCurrencyValue / euroTodayReal)
+        }
     }
 
+}
 
-convertButton.addEventListener("click", convertValues )
+
+convertButton.addEventListener("click", convertValues)
